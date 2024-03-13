3cb23b82a48355beab9e96d46d2baa6f89eb2dce
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-03-13 18:28:44 +0100

    REFACTOR

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 const gridElement = document.querySelector('.container')
-const playButton = document.querySelector('.btn')
-const Cells = 100
+const playButton = document.getElementById('play')
+const CellsEasy = 100
+const CellsNormal = 81
+const CellsHard = 49
 //al click del button devo togliere la classe d-none e aggiungere la classe
 //d-flex al container
-playButton.addEventListener('click',function(){
-gridElement.classList.remove("d-none")
+
+
+
+
+
+let i = 0
+playButton.addEventListener('click', function () {
+    while (i < 100) {
+    for (i = 0; i < CellsEasy; i++) {
+        let num = i + 1
+        
+            const cellEl = document.createElement('div')
+            cellEl.className = 'cell'
+            cellEl.innerHTML = num
+            // console.log(cellEl)
+            gridElement.append(cellEl)
+
+            cellEl.addEventListener('click', function () {
+                console.log('click sulla casella numero:', num)
+                cellEl.classList.toggle('bg-black')
+            })
+
+        }
+    }
 })
-
-
-
-
-for(let i = 0; i < Cells; i++){
-    let num = i + 1
-    const cellEl = document.createElement('div')
-    cellEl.className = 'cell'
-    cellEl.innerHTML = num
-    // console.log(cellEl)
-    gridElement.append(cellEl)
-
-    cellEl.addEventListener('click', function() {
-        console.log('click sulla casella numero:', num)
-        cellEl.classList.toggle('bg-black')
-    })
-    
-}
